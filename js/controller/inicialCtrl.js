3407ff01987d2442f1911cf4bbdd40de99d5919d
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2023-08-23 15:08:25 -0300

    Changed file to get just id and empresa.

--- js/controller/inicialCtrl.js
+++ js/controller/inicialCtrl.js
@@ -1,8 +1,7 @@
 app.controller("InicialCtrl", function ($scope, $http, $window, $rootScope) {
 
-	$scope.idempresa = $rootScope.idempresa = localStorage.getItem('idempresa');
+	$scope.id = $rootScope.idempresa = localStorage.getItem('id');
 	$scope.empresa = $rootScope.empresa = localStorage.getItem('empresa');
-	$scope.usuario = $rootScope.usuario = localStorage.getItem('usuario');
 
 	$scope.sair = function(){
 		localStorage.clear();
