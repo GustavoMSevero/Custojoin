4990dbe5cde43db181deacfac38b839ba2fcdba9
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2023-10-04 11:17:48 -0300

    Getting usuario.

--- js/controller/inicialCtrl.js
+++ js/controller/inicialCtrl.js
@@ -2,6 +2,7 @@ app.controller("InicialCtrl", function ($scope, $http, $window, $rootScope) {
 
 	$scope.id = $rootScope.idempresa = localStorage.getItem('id');
 	$scope.empresa = $rootScope.empresa = localStorage.getItem('empresa');
+	$scope.usuario = $rootScope.usuario = localStorage.getItem('usuario');
 
 	$scope.sair = function(){
 		localStorage.clear();
