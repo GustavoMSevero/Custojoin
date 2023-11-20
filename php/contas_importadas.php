044af967e3687252190f50cb99c48e613aca49ae
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2023-11-20 15:26:49 -0300

    Created contas importadas php file.

---
+++ php/contas_importadas.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+include_once("adminCJ/con.php");
+
+$pdo = conectar();
+
+$idempresa = $_GET['idempresa'];
+
+// echo $idempresa;
+
+$pegarContasImportadas=$pdo->prepare("SELECT * FROM conta_importada WHERE idempresa=:idempresa");
+$pegarContasImportadas->bindValue(":idempresa", $idempresa);
+$pegarContasImportadas->execute();
+
+while ($linha=$pegarContasImportadas->fetch(PDO::FETCH_ASSOC)) {
+
+    $dataP = explode('-', $linha['data']);
+    $data = $dataP[2].'/'.$dataP[1].'/'.$dataP[0];
+
+    $return[] = array(
+        'idconta'	=> $linha['idconta'],
+        'data'	=>  $data,
+        'codigo'	=> $linha['codigo'],
+        'tipo'	=> $linha['tipo'],
+        'valor'	=> $linha['valor'],
+        'descricao'	=> utf8_encode($linha['descricao']),
+    );
+
+}
+
+echo json_encode($return);
+
+
+
+?>
