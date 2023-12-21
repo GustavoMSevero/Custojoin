3b7c2a5906e58b9f1a096e481bd3de0a6c971be8
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2023-12-21 16:11:26 -0300

    Created controller for relatorios.

---
+++ js/controller/relatoriosCtrl.js
@@ -0,0 +1,22 @@
+app.controller("contas_importadasCtrl", function ($scope, $http, $rootScope) {
+
+	var idempresa = $rootScope.idempresa;
+	var empresa = $rootScope.empresa;
+	var usuario = $rootScope.usuario;
+
+	$scope.empresa = empresa;
+	$scope.usuario = usuario;
+
+    // var pegarContasImportadas = function() {
+    //     var opcao = 'pegar contas importadas';
+    //     $http.get("http://localhost:8880/web/Custojoin2/php/contas_importadas.php?opcao=" +opcao +"&idempresa="+ idempresa).success(function(data) {
+    //         // console.log(data)
+    //         $scope.contas_importadas = data;
+    //     })
+    // }
+    // pegarContasImportadas();
+
+
+});
+
+
