3b90233805eea1e0a4a1921e972f0b2d3e10b77a
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2023-12-27 15:17:05 -0300

    Removed commented code.

--- js/controller/relatoriosCtrl.js
+++ js/controller/relatoriosCtrl.js
@@ -6,22 +6,12 @@ app.controller("relatoriosCtrl", function ($scope, $http, $rootScope) {
 
 	$scope.empresa = empresa;
 	$scope.usuario = usuario;
-
-    // var pegarContasImportadas = function() {
-    //     var opcao = 'pegar contas importadas';
-    //     $http.get("http://localhost:8880/web/Custojoin2/php/contas_importadas.php?opcao=" +opcao +"&idempresa="+ idempresa).success(function(data) {
-    //         // console.log(data)
-    //         $scope.contas_importadas = data;
-    //     })
-    // }
-    // pegarContasImportadas();
     
     $scope.consultar = function(data) {
         data.idempresa = idempresa;
-        console.log(data)
+        // console.log(data)
         $http.post('http://localhost:8880/web/Custojoin2/php/relatorio_mensal.php', data).success(function(response) {
             console.log(response)
-            // $scope.reltorioMensal = data;
         });
     }
 
