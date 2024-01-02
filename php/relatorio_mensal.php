2898840da2e9946a48a6ee3673c143fea3a5cfcc
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2024-01-02 10:16:30 -0300

    Saving report once.

--- php/relatorio_mensal.php
+++ php/relatorio_mensal.php
@@ -10,32 +10,40 @@ $data = file_get_contents("php://input");
 $data = json_decode($data);
 
 $idempresa = $data->idempresa;
-$dia = $data->dia;
+// $dia = $data->dia;
 $mes = $data->mes;
 $ano = $data->ano;
 
-// $relatorioMensal=$pdo->prepare("SELECT MONTH(:mes), codigo, descricao, SUM(valor) 
-//                             AS valor_total 
-//                             FROM conta_importada 
-//                             WHERE idempresa=:idempresa 
-//                             GROUP BY data, codigo, descricao 
-//                             ORDER BY data, codigo");
-// $relatorioMensal->bindValue(":idempresa", $idempresa);
-// $relatorioMensal->bindValue(":mes", $mes);
-// $relatorioMensal->execute();
-
-$relatorioMensal=$pdo->prepare("INSERT INTO relatorios (mes, idempresa, codigo, descricao, valor_total)
-                                SELECT data, idempresa, codigo, descricao, SUM(valor) AS valor_total 
-                                FROM conta_importada 
+$checkIfExists=$pdo->prepare("SELECT *
+                                FROM relatorios 
                                 WHERE idempresa=:idempresa
-                                AND MONTH(data)=:mes
-                                AND YEAR(data)=:ano
-                                GROUP BY data, codigo, descricao 
-                                ORDER BY data, codigo");
-$relatorioMensal->bindValue(":idempresa", $idempresa);
-$relatorioMensal->bindValue(":mes", $mes);
-$relatorioMensal->bindValue(":ano", $ano);
-$relatorioMensal->execute();
+                                AND MONTH(mes)=:mes
+                                AND YEAR(mes)=:ano");
+$checkIfExists->bindValue(":idempresa", $idempresa);
+$checkIfExists->bindValue(":mes", $mes);
+$checkIfExists->bindValue(":ano", $ano);
+$checkIfExists->execute();
+
+$numOfRows = $checkIfExists->rowCount();
+
+if ($numOfRows > 0) {
+
+} else {
+
+    $monthlyReport=$pdo->prepare("INSERT INTO relatorios (mes, idempresa, codigo, descricao, valor_total)
+                                    SELECT data, idempresa, codigo, descricao, SUM(valor) AS valor_total 
+                                    FROM conta_importada 
+                                    WHERE idempresa=:idempresa
+                                    AND MONTH(data)=:mes
+                                    AND YEAR(data)=:ano
+                                    GROUP BY data, codigo, descricao 
+                                    ORDER BY data, codigo");
+    $monthlyReport->bindValue(":idempresa", $idempresa);
+    $monthlyReport->bindValue(":mes", $mes);
+    $monthlyReport->bindValue(":ano", $ano);
+    $monthlyReport->execute();
+}
+
 
 
 // while ($linha=$relatorioMensal->fetch(PDO::FETCH_ASSOC)) {
