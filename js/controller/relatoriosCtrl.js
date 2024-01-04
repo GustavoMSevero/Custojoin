8c07e3a245e40ea30773020169316e7aa38eaed2
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2024-01-04 16:17:49 -0300

    Bringing report data.

--- js/controller/relatoriosCtrl.js
+++ js/controller/relatoriosCtrl.js
@@ -11,11 +11,17 @@ app.controller("relatoriosCtrl", function ($scope, $http, $rootScope) {
         data.idempresa = idempresa;
         // console.log(data)
         $http.post('http://localhost:8880/web/Custojoin2/php/relatorio_mensal.php', data).success(function(response) {
-            console.log(response)
+            // console.log(response)
+            // console.log(response.mes)
+            // console.log(response.ano)
+            // console.log(idempresa)
+            var mes = response.mes;
+            var ano = response.ano;
+            $http.get('http://localhost:8880/web/Custojoin2/php/pegar_relatorio_mensal.php?idempresa='+idempresa+'&mes='+mes+'&ano='+ano).success(function(data) {
+                console.log(data)
+            })
         });
     }
 
 
 });
-
-
