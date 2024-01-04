61c9bdecb4058ac98fb76d9c9d65304659d1a663
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2024-01-04 16:19:27 -0300

    Created php file to bring report data.

---
+++ php/pegar_relatorio_mensal.php
@@ -0,0 +1,45 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+include_once("adminCJ/con.php");
+
+$pdo = conectar();
+
+$idempresa = $_GET['idempresa'];
+$mes = $_GET['mes'];
+$ano = $_GET['ano'];
+
+$getReport=$pdo->prepare("SELECT *
+                        FROM relatorios 
+                        WHERE idempresa=:idempresa
+                        AND MONTH(mes)=:mes
+                        AND YEAR(mes)=:ano");
+$getReport->bindValue(":idempresa", $idempresa);
+$getReport->bindValue(":mes", $mes);
+$getReport->bindValue(":ano", $ano);
+$getReport->execute();
+
+
+while ($linha=$getReport->fetch(PDO::FETCH_ASSOC)) {
+
+    $codigo = $linha['codigo'];
+    $descricao = $linha['descricao'];
+    $valor_total = $linha['valor_total'];
+
+    $valor_total = number_format($valor_total,2,",",".");
+
+    $return[] = array(
+        'codigo'	=> $codigo,
+        'descricao'	=> $descricao,
+        'valor_total'	=> $valor_total
+    );
+
+}
+
+echo json_encode($return);
+
+
+
+
+?>
